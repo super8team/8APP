053e4351cefaab9cd15af028ded462523944b724
kimill1394 <kimill1394@gmail.com>
2017-09-26 14:32:45 +0900

    set domain

--- app/src/main/java/com/learnfun/super8team/learnfun/Async/NetworkAsync.java
+++ app/src/main/java/com/learnfun/super8team/learnfun/Async/NetworkAsync.java
@@ -38,7 +38,7 @@ public class NetworkAsync extends AsyncTask<String, Integer, String> {
 
     Context context = null;
     // 서비스도메인
-    String urlAddr = "http://163.44.166.91/8server/public/app/";
+    String urlAddr = "http://learnfun8.xyz/app/";
     JSONObject jsonObject = new JSONObject();
     String httpMethod;
 
